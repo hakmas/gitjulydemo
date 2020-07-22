bb28a4d3b76d51912a18ea3c29bf8811cf0fdd90
Ilkin Wober <ilkin@ilkins-mbp.attlocal.net>
2020-07-21 19:34:47 -0500

    adding new statement

---
+++ src/HelloGit.java
@@ -0,0 +1,12 @@
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+//		System.out.println("My second statement");
+//		System.out.println("My third statement");
+		
+
+	}
+
+}
